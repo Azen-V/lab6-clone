6ee3d096ce29ef905616dfc2cfa438370f70d31a
Azen-V <kuaboy34@gmail.com>
2025-12-12 23:49:05 +0700

    Update lab6_1.cpp

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int evennumber = 0;
+    int obbnumber = 0;
+    int number;
+    do{
+        cout << "Enter an integer: ";
+        cin >> number;
+        if(number == 0){
+            break;
+        }
+        if(number % 2 == 0){evennumber++;}
+        else{obbnumber++;}
+    }
+    while (true);
+    cout << "#Even numbers = "<<evennumber;
+    cout << "#Odd numbers = "<<obbnumber;
     return 0;
 }
